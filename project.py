cd2ceb6167e350cc4067d452fff7d0183d472692
Changhwan Kim <kimch061279@gmail.com>
2024-11-24 13:18:15 +0900

    edit project.py; add init_file_system func

--- project.py
+++ project.py
@@ -1,6 +1,26 @@
 from fastapi import APIRouter, HTTPException
-from fastapi.responses import JSONResponse
 from pydantic import BaseModel
-import sys, os, requests
+import os
 
 router = APIRouter()
+
+class project_init(BaseModel):
+    PUID: int
+
+@router.post("/project/init")
+async def init_file_system(payload: project_init):
+    base_path = "/data/PMS"
+    puid = str(payload.PUID)
+    root_path = os.path.join(base_path, puid)
+
+    folder_structure = ["WBS","MM","SD","OD","RS","UT","IT","ETC"]
+
+    try:
+        os.makedirs(root_path, exist_ok=True)
+        for folder in folder_structure:
+            folder_path = os.path.join(root_path, folder)
+            os.makedirs(folder_path, exist_ok=True)
+    except Exception as e:
+        return {"RESULT_CODE": 500, "RESULT_MSG": str(e)}
+
+    return True
