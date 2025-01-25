433de30760d90b55e15d4842ab2b8bba089e5558
Changhwan Kim <kimch061279@gmail.com>
2025-01-25 17:00:15 +0900

    edit ccp.py; edit functions

--- ccp.py
+++ ccp.py
@@ -2,10 +2,16 @@ from fastapi import APIRouter, HTTPException, File, UploadFile, Form, Query
 from fastapi.responses import FileResponse
 from pydantic import BaseModel
 from datetime import datetime
-import os
+import os, shutil, tarfile
 
 router = APIRouter()
 
 @router.post("/ccp/push")
-async def api_output_push():
-    return
+async def api_output_push(pid: int):
+    remote_folder = f'/data/PMS/{pid}'
+    archive_path = f'/tmp/{pid}_output.tar.gz'
+
+    with tarfile.open(archive_path, 'w:gz') as tar:
+        tar.add(remote_folder, arcname=os.path.basename(remote_folder))
+
+    return FileResponse(archive_path, media_type='application/gzip', filename=f"{pid}_output.tar.gz")
