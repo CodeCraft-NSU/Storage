2a0abb0e6aeab33967ba015acf65e1a538483137
Changhwan Kim <kimch061279@gmail.com>
2025-02-04 21:35:54 +0900

    edit ccp.py; add api_ccp_pull func

--- ccp.py
+++ ccp.py
@@ -28,6 +28,17 @@ async def api_ccp_push(pid: int):
     return FileResponse(archive_path, media_type='application/gzip', filename=f"{pid}_output.tar.gz")
 
 @router.post("/ccp/pull")
-async def api_ccp_pull(pid: int):
-    init_result = init_ccp_folder(str(pid))
-    if not init_result: raise HTTPException(status_code=500, detail=f"Failed to initialize folder")
+async def api_ccp_pull(
+    pid: int,
+    data: UploadFile = File(...),
+    name: str = Form(...)
+):
+    if not init_ccp_folder(str(pid)):
+        raise HTTPException(status_code=500, detail=f"Failed to initialize folder")
+    file_path = f"/data/CCP/{pid}/{name}.ccp"
+    try:
+        with open(file_path, "wb") as f:
+            f.write(await data.read())
+        return {"RESULT_CODE": 200,"RESULT_MSG": "File uploaded successfully"}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error during file upload: {str(e)}")
